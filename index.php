ebce4e459303f8755cbd51e602119c845fbc6a30
Gekif <dzulfikar.maulana@gmail.com>
2018-10-07 10:37:43 +0700

    TokoFikarSCV.1.2: Complete Main Page

--- index.php
+++ index.php
@@ -1,13 +1,9 @@
 <!doctype html>
-<html lang="en">
+<html>
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport"
-          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Toko Fikar</title>
-    <link rel="stylesheet" href="style.css">
     <link rel="stylesheet" href="//maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css">
+    <link rel="stylesheet" href="style.css">
 </head>
 <body>
 <nav class="navbar navbar-inverse">
@@ -30,7 +26,7 @@
 <div class="container">
     <div class="row">
         <div class="col-md-3">
-            <div class="col-md-12">
+            <div class="col-md-12 single-item noPadding">
                 <div class="top"></div>
                 <div class="bottom">
                     <h3 class="item-title">Beautiful Watch</h3>
@@ -40,10 +36,97 @@
                 </div>
             </div>
         </div>
-
+        <div class="col-md-3">
+            <div class="col-md-12 single-item noPadding">
+                <div class="top"></div>
+                <div class="bottom">
+                    <h3 class="item-title">Beautiful Watch</h3>
+                    <div class="pull-right cutted-price text-muted"><del>Rp. 500.000</del></div>
+                    <div class="clearfix"></div>
+                    <div class="pull-right discounted-price">Rp. 450.000</div>
+                </div>
+            </div>
+        </div>
+        <div class="col-md-3">
+            <div class="col-md-12 single-item noPadding">
+                <div class="top"></div>
+                <div class="bottom">
+                    <h3 class="item-title">Beautiful Watch</h3>
+                    <div class="pull-right cutted-price text-muted"><del>Rp. 500.000</del></div>
+                    <div class="clearfix"></div>
+                    <div class="pull-right discounted-price">Rp. 450.000</div>
+                </div>
+            </div>
+        </div>
+        <div class="col-md-3">
+            <div class="col-md-12 single-item noPadding">
+                <div class="top"></div>
+                <div class="bottom">
+                    <h3 class="item-title">Beautiful Watch</h3>
+                    <div class="pull-right cutted-price text-muted"><del>Rp. 500.000</del></div>
+                    <div class="clearfix"></div>
+                    <div class="pull-right discounted-price">Rp. 450.000</div>
+                </div>
+            </div>
+        </div>
+        <div class="col-md-3">
+            <div class="col-md-12 single-item noPadding">
+                <div class="top"></div>
+                <div class="bottom">
+                    <h3 class="item-title">Beautiful Watch</h3>
+                    <div class="pull-right cutted-price text-muted"><del>Rp. 500.000</del></div>
+                    <div class="clearfix"></div>
+                    <div class="pull-right discounted-price">Rp. 450.000</div>
+                </div>
+            </div>
+        </div>
+        <div class="col-md-3">
+            <div class="col-md-12 single-item noPadding">
+                <div class="top"></div>
+                <div class="bottom">
+                    <h3 class="item-title">Beautiful Watch</h3>
+                    <div class="pull-right cutted-price text-muted"><del>Rp. 500.000</del></div>
+                    <div class="clearfix"></div>
+                    <div class="pull-right discounted-price">Rp. 450.000</div>
+                </div>
+            </div>
+        </div>
+        <div class="col-md-3">
+            <div class="col-md-12 single-item noPadding">
+                <div class="top"></div>
+                <div class="bottom">
+                    <h3 class="item-title">Beautiful Watch</h3>
+                    <div class="pull-right cutted-price text-muted"><del>Rp. 500.000</del></div>
+                    <div class="clearfix"></div>
+                    <div class="pull-right discounted-price">Rp. 450.000</div>
+                </div>
+            </div>
+        </div>
+        <div class="col-md-3">
+            <div class="col-md-12 single-item noPadding">
+                <div class="top"></div>
+                <div class="bottom">
+                    <h3 class="item-title">Beautiful Watch</h3>
+                    <div class="pull-right cutted-price text-muted"><del>Rp. 500.000</del></div>
+                    <div class="clearfix"></div>
+                    <div class="pull-right discounted-price">Rp. 450.000</div>
+                </div>
+            </div>
+        </div>
     </div>
 </div>
 
+<div class="clearfix"></div>
+<footer class="navbar navbar-inverse navbar-fixed-bottom">
+    <div class="container-fluid">
+        <ul class="nav navbar-nav">
+            <li><a href="#">Term and Condition</a></li>
+            <li><a href="#">About Us</a></li>
+            <li><a href="#">Privacy</a></li>
+            <li><a href="#">Return Policy</a></li>
+        </ul>
+    </div>
+</footer>
 
 <script src="//maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"></script>
 <script src="http://code.jquery.com/jquery-1.12.0.js" integrity="sha256-yFU3rK1y8NfUCd/B4tLapZAy9x0pZCqLZLmFL3AWb7s=" crossorigin="anonymous"></script>
